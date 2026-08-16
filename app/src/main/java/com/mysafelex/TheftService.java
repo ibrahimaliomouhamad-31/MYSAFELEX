736f24d7cdae3bdecb5e7ae611143a348c86f7de
Mouhamad <ibrahimaliomouhamad@gmail.com>
2026-08-16 13:15:43 +0100

    Update TheftService.java

--- app/src/main/java/com/mysafelex/TheftService.java
+++ app/src/main/java/com/mysafelex/TheftService.java
@@ -34,6 +34,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import androidx.core.app.ActivityCompat;
+import android.Manifest;
 
 public class TheftService extends Service {
 
